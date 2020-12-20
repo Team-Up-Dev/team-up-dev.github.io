cd925e3e6c93cb0721712cddafebb5bb207ff413
WazeAzure <bryan.rich0604@gmail.com>
2020-12-20 14:44:34 +0800

    Small Changes and adding Card

---
+++ src/components/Card/Card.js
@@ -0,0 +1,34 @@
+import "./Card.scss";
+import list from "./Profile"
+const profileList = list.List;
+
+function CreateCards(card,index){
+	return (
+		<div className = "cards">
+			<img src={card.picture} alt="scenery"></img>
+			<div className="cards-text">
+				<p>{card.nama}</p>
+				<ul>{card.tags.map((tag,index) => {
+					return (<li className={tag.toLowerCase()}>{tag}</li>)})
+					}
+				</ul>
+				<a href={card.url} id="get-detail">Get Detail >></a>
+			</div>
+		</div>
+	);
+
+}
+
+const Card = () => {
+	return(
+		<>
+			<div className="cards-box">
+				<div className="cards-box-container" id="cards-list">
+					{profileList.map(CreateCards)}
+				</div>
+			</div>
+		</>
+	);
+}
+export default Card;
+	
