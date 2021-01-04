c83a37f818448f8ab501c2d9926f95e6f3b2c367
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-04 22:14:27 +0700

    Use loadable component and refactor code

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -1,24 +1,32 @@
-import Navbars from "./Navbar";
-import Home from "../containers/Home";
-import Profile from "../containers/Profile";
-import Discussion from "../containers/Discussion";
-import Project from "../containers/Project";
-import Training from "../containers/Training";
-import NotFound from "../containers/NotFound";
-
-import React from "react";
+import loadable from "@loadable/component";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+const Navbars = loadable(() => import("./Navbar"), {
+  fallback: (
+    <nav
+      className="navbar navbar-expand-lg navbar-dark bg-dark"
+      style={{ height: "56px" }}
+    />
+  )
+});
+
+const Home = loadable(() => import("../containers/Home"));
+const Profile = loadable(() => import("../containers/Profile"));
+const Discussion = loadable(() => import("../containers/Discussion"));
+const Project = loadable(() => import("../containers/Project"));
+const Training = loadable(() => import("../containers/Training"));
+const NotFound = loadable(() => import("../containers/NotFound"));
 
 export default function App() {
   return (
     <Router>
       <Navbars />
       <Switch>
-        <Route path="/Profile" component={Profile} />
-        <Route path="/Discussion" component={Discussion} />
-        <Route path="/project" component={Project} />
-        <Route path="/training" component={Training} />
         <Route exact path="/" component={Home} />
+        <Route path="/Discussion" component={Discussion} />
+        <Route path="/training" component={Training} />
+        <Route path="/project" component={Project} />
+        <Route path="/Profile" component={Profile} />
         <Route component={NotFound} />
       </Switch>
     </Router>
