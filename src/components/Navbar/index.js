c855b14a0e3a378c24cdc59e1ba8bf7983e85420
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-04 22:38:52 +0700

    Use foreach and add activeKey

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,40 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import { Nav } from "react-bootstrap";
 import logo from "./brand-icon.png";
+
+const Navigate = ["Profile", "Discussion", "Project", "Training"];
+
 const Navbars = () => {
+  const location = useLocation();
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Link>
-        <Navbar.Brand as={Link} to="/">
-          <img
-            alt=""
-            src={logo}
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-          />{" "}
-          Team up
-        </Navbar.Brand>
-      </Link>
+      <Navbar.Brand as={Link} to="/">
+        <img
+          alt="Team Up Dev Logo"
+          src={logo}
+          width="30"
+          height="30"
+          className="d-inline-block align-top"
+        />{" "}
+        Team up
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/">
-            HomePage
-          </Nav.Link>
-          <Nav.Link as={Link} to="/profil">
-            Profile
-          </Nav.Link>
-          <Nav.Link as={Link} to="/discussion">
-            Discussion
-          </Nav.Link>
-          <Nav.Link as={Link} to="/project">
-            Project
-          </Nav.Link>
-          <Nav.Link as={Link} to="/training">
-            Training
-          </Nav.Link>
+        <Nav className="mr-auto" activeKey={location.pathname}>
+          {Navigate.map((navi) => {
+            const to = "/" + navi.toLowerCase();
+
+            return (
+              <Nav.Link key={navi} as={Link} to={to} href={to}>
+                {navi}
+              </Nav.Link>
+            );
+          })}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
