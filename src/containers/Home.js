dd3e1b1ffb72d2f27232e1454555cc667de8dc40
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-05 09:56:03 +0700

    Rename file

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
 import loadable from "@loadable/component";
 import Carousel from "../components/Carousel/index";
 
-const CardList = loadable(() => import("../components/Card/Card"));
-const Footer = loadable(() => import("../components/Footer/Footer"));
+const CardList = loadable(() => import("../components/Card"));
+const Footer = loadable(() => import("../components/Footer"));
 
 const Home = () => {
   return (
